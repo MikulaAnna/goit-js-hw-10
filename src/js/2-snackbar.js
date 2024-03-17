c784b3a014482c724fe89a9e8fde96bf27328c64
MikulaAnna <mikula.v.anna@gmail.com>
2024-03-17 16:07:17 +0200

    fix: js code

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,49 +1,37 @@
-// INCLUDING LIBRARY IZITOAST
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('form');
+const form = document.querySelector('.form');
 
-form.addEventListener('submit', generatePromises);
+form.addEventListener('submit', generatePromise);
 
-function generatePromises(event) {
+function generatePromise(event) {
   event.preventDefault();
 
   const delay = parseInt(event.currentTarget.elements.delay.value);
   const state = event.currentTarget.elements.state.value;
 
   setTimeout(() => {
-    let promise;
-
-    promise = new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
       if (state === 'fulfilled') {
-        setTimeout(() => {
-          resolve(delay);
-        }, delay);
+        resolve(delay);
       } else if (state === 'rejected') {
-        setTimeout(() => {
-          reject(delay);
-        }, delay);
+        reject(delay);
       }
     });
 
     promise
       .then(delay => {
         iziToast.success({
-          timeout: false,
-          message: `Fulfilled promise in ${delay} ms`,
-          messageColor: 'rgb(255, 255, 255)',
-          messageSize: '16px',
-          backgroundColor: 'rgb(89, 161, 13)',
-          maxWidth: '384px',
+          title: 'Notification',
+          message: `✅ Fulfilled promise in ${delay}ms`,
           position: 'topRight',
-          progressBarColor: 'rgb(50, 97, 1)',
         });
       })
       .catch(delay => {
         iziToast.error({
-          message: `Rejected promise in ${delay}ms`,
+          title: 'Notification',
+          message: `❌ Rejected promise in ${delay}ms`,
           position: 'topRight',
         });
       });
